5e351c7ee50bd72c388afefdfb35e2f2c0a84e5c
ngochuyvu <128366057+NgocHuyVu@users.noreply.github.com>
2024-02-25 18:34:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,76 @@ class CalculatorWidget(Screen):
     def vymaz_jedno(self):
         self.ids.vstup.text = self.ids.vstup.text[:-1]
 
+    def cisla(self, cislo): #funkce na cisla
+        vstup_cisla = self.ids.vstup.text
+        vystup_cisla = self.ids.vystup.text
+
+        if vstup_cisla == "0":
+            self.ids.vstup.text = ""
+            self.ids.vstup.text += (f"{cislo}")
+        else:
+            self.ids.vstup.text  = (f"{vstup_cisla}{cislo}") #pridava cisla
+    
+    def operace(self, znak):
+        znaky = ["+", "-", "*", "/", "."]
+        vstup_znaku = self.ids.vstup.text
+        vystup_znaku = self.ids.vystup.text
+
+        if CalculatorWidget.temp_cislo != "":
+            self.ids.vstup.text  = (f"{CalculatorWidget.temp_cislo}{znak}")
+            CalculatorWidget.temp_cislo = ""
+        else:
+            if (len(vstup_znaku) <= 1 and vstup_znaku == "0"): #resi znamenka
+                pass
+            else: #zmena znamenek pri duplicitnim pouziti
+                if vstup_znaku[-1] in znaky:
+                    self.ids.vstup.text = vstup_znaku[:-1] + znak
+                else:
+                    self.ids.vstup.text  = (f"{vstup_znaku}{znak}")
+    
+    def vypocitej(self):
+        vstup = self.ids.vstup.text
+        try:
+            self.ids.vystup.text = str(eval(vstup))
+            self.ids.vstup.text  = self.ids.vstup.text + "="
+            CalculatorWidget.temp_cislo = self.ids.vystup.text
+            #print(CalculatorWidget.temp_cislo)
+        except:
+            self.ids.vystup.text = "Error"
+            self.ids.vstup.text = "0" #automaticky to zmeni vstup na nulu
+    
+    def zmen_rezim(self):
+        for button_id in self.ids.keys():
+            button = self.ids[button_id]
+            if isinstance(button, Button):
+                if button.background_color == [0, 1, 1, 1]:
+                    button.background_color = [0.7, 0.7, 0.7, 1]
+                else:
+                    button.background_color = [0, 1, 1, 1]
+    
+    def vedecka_kalkulacka_velikost(self):
+        if CalculatorWidget.rezim_velikost == 0:
+            Window.size = (600,350)
+            CalculatorWidget.rezim_velikost = 1
+            #self.sm.current = "sc_kalkulacka"
+            #Builder.unload_file("./my.kv")
+        else:
+            Window.size = (350,600)
+            CalculatorWidget.rezim_velikost = 0
+
+class ScienceCalculatorWidget(Screen):
+    temp_cislo = "" #pro to abychom mohli tamto cos chtel
+    #rezim_barva = 0
+    rezim_velikost = 0
+
+    def smaz(self): #funkce na mazani, je tam chyba
+        self.ids.vstup.text = "0"
+        self.ids.vystup.text = ""
+        CalculatorWidget.temp_cislo = ""
+
+    def vymaz_jedno(self):
+        self.ids.vstup.text = self.ids.vstup.text[:-1]
+
     def vedecka_kalkulacka(self, operace):
         vstup_cisla = self.ids.vstup.text
 
@@ -99,9 +169,6 @@ class CalculatorWidget(Screen):
         else:
             Window.size = (350,600)
             CalculatorWidget.rezim_velikost = 0
-
-class ScienceCalculatorWidget(Screen):
-    pass
 
 class CalculatorManager(ScreenManager):
     pass

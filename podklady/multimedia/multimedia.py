2860e29e0d237dd0a47fa105e7bbdad50bf47811
GodComplexLIT <greenfireczone@gmail.com>
2024-02-27 23:21:24 +0100

    podklady

--- podklady/multimedia/multimedia.py
+++ podklady/multimedia/multimedia.py
@@ -1,8 +1,16 @@
 from kivy.app import App
 from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
 
 class Priklad(App):
     def build(self):
+        #return Image(source='pan.jpg')
+        zvuk = SoundLoader.load('bird.mp3')
+        if zvuk:
+            print(f"Délka zvuku: {zvuk.length}")
+            zvuk.play()
+        #return Label(text='Hello world')
         return Image(source='pan.jpg')
 
 Priklad().run()

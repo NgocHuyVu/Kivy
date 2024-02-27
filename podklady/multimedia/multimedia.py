a3a2c31d1138d30b3cd2fa00097f26811c61f7e6
GodComplexLIT <greenfireczone@gmail.com>
2024-02-27 23:59:23 +0100

    podklady

--- podklady/multimedia/multimedia.py
+++ podklady/multimedia/multimedia.py
@@ -2,7 +2,11 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.core.audio import SoundLoader
+from kivy.uix.video import Video
+from kivy.lang.builder import Builder
 
+kv = Builder.load_file("./multimedia.kv")
+'''
 class Priklad(App):
     def build(self):
         #return Image(source='pan.jpg')
@@ -12,5 +16,16 @@ class Priklad(App):
             zvuk.play()
         #return Label(text='Hello world')
         return Image(source='pan.jpg')
+'''
 
+'''
+class Priklad(App):
+    def build(self):
+        return Video(source="dog.mp4")
+'''
+
+class Priklad(App):
+    def build(self):
+        return kv
+    
 Priklad().run()
